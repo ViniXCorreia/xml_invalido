200f04e548123032c061ec3cdf3348d490bb1ca8
Vinicus Martins Correia <vinix.correia@gmail.com>
2021-09-30 19:09:01 -0300

    2.3

--- AjustaXML.py
+++ AjustaXML.py
@@ -2,6 +2,7 @@ from pathlib import Path
 from tkinter import *
 from tkinter import messagebox
 from tkinter.filedialog import askopenfilename
+import webbrowser
 import os
 import shutil
 
@@ -18,23 +19,30 @@ class Application:
       self.busca["height"] = 2
       self.busca["bd"] = 3
       self.busca["command"] = self.buscarArquivo
-      self.busca.grid(row=2, column=3, sticky= N)
+      self.busca.grid(row=3, column=3, sticky= N)
+      self.portalNfe = Button(self.widget1)
+      self.portalNfe["text"] = "Acessar Consulta NFe"
+      self.portalNfe["font"] = ("Calibri", "12")
+      self.portalNfe["bd"] = 0
+      self.portalNfe["fg"] = "Blue"
+      self.portalNfe["command"] = self.openSite
+      self.portalNfe.grid(row = 1, column=3)
       self.checkButton = Checkbutton(self.widget1)
       self.checkButton["text"] = "Manter arquivo XML original"
       self.checkButton["height"] = 2
       self.checkButton["font"] = ("Calibri", "12")
       self.checkButton["variable"] = valor_check
-      self.checkButton.grid(row = 1, column= 3, sticky = N)
+      self.checkButton.grid(row = 2, column= 3, sticky = N)
       self.vazio = Label(self.widget1)
       self.vazio["text"] = " "
-      self.vazio.grid(row = 4, column = 3)
+      self.vazio.grid(row = 5, column = 3)
       self.cancela = Button(self.widget1)
       self.cancela["text"] = "Finalizar"
       self.cancela["font"] = ("Calibri", "10")
       self.cancela["width"] = 25
       self.cancela["height"] = 2
       self.cancela["command"] = self.finalizaArquivo
-      self.cancela.grid(row = 5, column=3, sticky= N)
+      self.cancela.grid(row = 6, column=3, sticky= N)
  
    def buscarArquivo(self):
       Tk().withdraw()
@@ -62,10 +70,8 @@ class Application:
             self.ajustaXML(filename, caminhoChaveXML, chaveXML)
          else:
             messagebox.showerror("Ajuste XML", "Nome do arquivo inválido! \nVerifique se existem apenas 44 números na chave de acesso!")
-            #self.vazio["text"] = "Nome do arquivo inválido! Verifique se existem apenas 44 números na chave de acesso!"
       elif len(chaveXML[-1]) > 8 and len(chaveXML[-1]) < 52:
          messagebox.showerror("Ajuste XML", "Nome do arquivo inválido! \nEstá faltando números na chave de acesso!")
-         #self.vazio["text"] = "Nome do arquivo inválido! Está faltando números na chave de acesso!"
 
    def ajustaXML(self, filename, caminhoChaveXML, chaveXML):
          os.rename(filename, caminhoChaveXML)
@@ -89,7 +95,6 @@ class Application:
                         path_nfe_copia = os.path.join(raiz, pasta)
                         shutil.copy(caminhoChaveXML, path_nfe_copia + "/" + chaveXML)
                         messagebox.showinfo("Ajuste XML", "Arquivo XML ajustado com sucesso! \nConsulte novamente sua NFe!")
-                        #self.vazio["text"] = "Arquivo XML ajustado com sucesso!"
                         verificaNfe = True
       if not verificaLog:
          newDir = os.path.join(dirUsado, "Log")
@@ -98,14 +103,15 @@ class Application:
          os.mkdir(newDirNFe)
          messagebox.showwarning("Ajuste XML", "Pastas Log e Nfe criadas!")
          self.copiar_xml(caminhoChaveXML, chaveXML)
-         #self.vazio["text"] = "Pasta Log e NFe criadas! Arquivo XML ajustado com sucesso"
       elif not verificaNfe:
          messagebox.showerror("Ajuste XML", "Pasta Nfe não encontrada! \nEntre em contato com o Suporte Técnico!")
-         #self.vazio["text"] = "Pasta NFe não encontrada, entre em contato com o Suporte Técnico!"
       
    
    def apaga_xml(self, filename):
       os.remove(filename)
+   
+   def openSite(self):
+      webbrowser.open("https://www.nfe.fazenda.gov.br/portal/consultaRecaptcha.aspx?tipoConsulta=resumo&tipoConteudo=d09fwabTnLk=")
 
    def finalizaArquivo(self):
       root.quit()
@@ -113,7 +119,7 @@ class Application:
 root = Tk()
 root.title("Ajuste XML")
 width_window = 550
-height_window = 500
+height_window = 530
 screen_width = root.winfo_screenwidth()
 screen_height = root.winfo_screenheight()
 posx = screen_width/2 - width_window/2
